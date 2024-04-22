a6a3afc7b3e5f590f327ba0e6bf106d1a0bb0651
DoanTrongQuan <trongquan202@gmail.com>
2024-04-22 20:29:25 +0700

    .

--- src/main/java/org/example/project_cinemas_java/controller/MessageController.java
+++ src/main/java/org/example/project_cinemas_java/controller/MessageController.java
@@ -1,14 +1,14 @@
-package org.example.project_cinemas_java.controller;
-
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class MessageController {
-    @MessageMapping("/hello")
-    @SendTo("/topic/messages")
-    public String send(String username) {
-        return "Hello, " + username;
-    }
-}
+//package org.example.project_cinemas_java.controller;
+//
+//import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.messaging.handler.annotation.SendTo;
+//import org.springframework.stereotype.Controller;
+//
+//@Controller
+//public class MessageController {
+//    @MessageMapping("/hello")
+//    @SendTo("/topic/messages")
+//    public String send(String username) {
+//        return "Hello, " + username;
+//    }
+//}
